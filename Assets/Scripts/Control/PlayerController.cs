ec48322cb5e0b4a2a0396db1035520d3aa22e08a
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-07-02 17:41:30 +0900

    enemy
    캐릭터 이동 완료

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<Mover>().MoveTo(hit.point);
+        }
+    }
+}
