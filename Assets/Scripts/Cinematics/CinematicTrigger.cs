21beec2ecd463dc6ff3b7de8fd22bd4057b1b468
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-07-18 14:36:48 +0900

    타임라인 65

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool isStart = false;
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.tag == "Player")
+            {
+                if(!isStart)
+                {
+                    GetComponent<PlayableDirector>().Play();
+                    isStart = true;
+                }
+
+            }
+
+        }
+    }
+}
+
